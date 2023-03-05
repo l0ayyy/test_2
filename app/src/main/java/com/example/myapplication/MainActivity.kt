892c701c0f6edfe42ddb66aa0e455acbe65fb851
l0ayyy <msido11@students.iugaza.edu.ps>
2023-03-05 17:44:11 +0200

    commit2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,55 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    val db = Firebase.firestore
+    lateinit var binding: ActivityMainBinding
+    lateinit var listView: ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        var x = "لؤي"
+        binding.save.setOnClickListener {
+            val name = binding.PersonName.text.toString()
+            val number = binding.Personnumber.text.toString()
+            val adddres = binding.Personaddress.text.toString()
+
+            val contact = hashMapOf(
+                "name" to name,
+                "number" to number,
+                "address" to adddres,
+            )
+            db.collection("contacts")
+                .add(contact)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                }.addOnFailureListener { e ->
+                    Log.w(TAG, "Error adding document", e)
+                }
+
+
     }
+        binding.get.setOnClickListener {
+            val i = Intent(this@MainActivity, MainActivity2::class.java)
+            startActivity(i)
+        }
+
+    }
+
 }
+
+
